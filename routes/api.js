b807c714d2eccad6776bb2b359e612c2c41c1822
Gianluca <gianlucazin1990@gmail.com>
2021-01-11 22:03:11 +0100

    post request updated

--- routes/api.js
+++ routes/api.js
@@ -87,25 +87,25 @@ module.exports = function (app) {
     .post(async function (req, res) {
       try {
         let project = req.params.project;
-        let {
-          issue_title,
-          issue_text,
-          created_by,
-          assigned_to,
-          status_text,
-        } = req.body;
-
-        if (!project || !issue_title || !issue_text || !created_by)
-          res.json({ error: 'required field(s) missing' });
 
         let newIssue = await new Issue({
-          issue_title,
-          issue_text,
-          created_by,
-          assigned_to,
-          status_text,
+          issue_title: req.body.issue_title,
+          issue_text: req.body.issue_text,
+          created_by: req.body.created_by,
+          assigned_to: req.body.assigned_to,
+          status_text: req.body.status_text,
+          open: true,
+          created_on: new Date().toUTCString(),
+          updated_on: new Date().toUTCString(),
           project,
         });
+        if (
+          !project ||
+          !req.body.issue_title ||
+          !req.body.issue_text ||
+          !req.body.created_by
+        )
+          res.json({ error: 'required field(s) missing' });
 
         newIssue.save((err, savedIssue) => {
           if (!err && savedIssue) {
